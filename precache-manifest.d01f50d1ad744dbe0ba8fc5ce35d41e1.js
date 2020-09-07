93ee250066610b54c55c59367e2958c3a2288d43
jay yadav <jayprakash9867@gmail.com>
2020-09-07 19:07:48 +0530

    Updates

---
+++ precache-manifest.d01f50d1ad744dbe0ba8fc5ce35d41e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2eb54cdeb0ebe97e50bb",
+    "url": "/contactBook/static/css/main.4420aedb.chunk.css"
+  },
+  {
+    "revision": "2eb54cdeb0ebe97e50bb",
+    "url": "/contactBook/static/js/main.e7063017.chunk.js"
+  },
+  {
+    "revision": "d08ea179c5f1485d1d8c",
+    "url": "/contactBook/static/js/runtime~main.bc6ee4aa.js"
+  },
+  {
+    "revision": "7ae035c5b8f77c0f0b1f",
+    "url": "/contactBook/static/css/2.ccff64e2.chunk.css"
+  },
+  {
+    "revision": "7ae035c5b8f77c0f0b1f",
+    "url": "/contactBook/static/js/2.276b9828.chunk.js"
+  },
+  {
+    "revision": "38cd71d4b811974a4e10ab7432f71be9",
+    "url": "/contactBook/index.html"
+  }
+];
